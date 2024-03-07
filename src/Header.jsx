1e95c6c8a42efa6c1d0f63e46a070392abe08737
Brennan Duck <brennantduck@gmail.com>
2024-03-07 11:33:18 -0500

    added useful text

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
 function Header() {
     return (
         <header className="text-center my-10">
-            <h1 className="font-extrabold text-3xl text-nord4">
-                React Pokedex
-            </h1>
+            <h1 className="font-extrabold text-3xl text-nord4">React Pokedex</h1>
+            <h2 className="font-thin text-xl text-nord4">
+                most useful and productive application since the invention of the
+                internet
+            </h2>
             <a
                 className="font-semibold underline-offset-2 underline hover:text-nord-3 text-nord4 text-l"
                 href="https://github.com/bduckdev/react-pokedex"
